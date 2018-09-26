3d33c1fcbd8f92ce3632ef7d5e763eea7636e193
Boaz Rosenan <brosenan@gmail.com>
2018-09-26 20:59:07 +0300

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject brosenan/mariadb-event-store "0.1.0"
+(defproject brosenan/mariadb-event-store "0.1.1-SNAPSHOT"
   :description "An EventStoreService implementation based on an array of MariaDB instances"
   :url "https://github.com/brosenan/mariadb-event-store"
   :license {:name "Eclipse Public License"
