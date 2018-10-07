e6600d9d6dae434a38e50300978187ed799216e7
Boaz Rosenan <brosenan@gmail.com>
2018-10-07 21:34:42 +0300

    deployment for waiting for all database nodes

--- src/mariadb_event_store/lk.clj
+++ src/mariadb_event_store/lk.clj
@@ -49,7 +49,22 @@
                        (lk/add-annotation :num-shards num-shards)
                        (lk/add-annotation :num-replicas replication-factor)
                        (lku/add-itd-annotations MariaDBEventStoreService "https://github.com/brosenan/mariadb-event-store/raw/master/mariadb-event-store-uber.jar")
-                       (lk/expose-headless :mariadb-event-store (lk/port :db :mysql mysql-port mysql-port))))))))
+                       (lk/expose-headless :mariadb-event-store (lk/port :db :mysql mysql-port mysql-port))
+                       ;; An deployment to detect when all database
+                       ;; nodes are active, and then open a socket to
+                       ;; allow other pods to wait for it.
+                       (update :$additional conj
+                               (-> (lk/pod :wait-for-all-dbs {:belongs-to :mdb-es})
+                                   (lk/add-init-container :wait-for-dbs "busybox"
+                                                          (-> {:command ["sh" "-c" "while ! sh -e /etc/wait-for-db/scan-dbs.sh; do sleep 1; done"]}
+                                                              (lk/add-env {:NUM_NODES (str (dec (* replication-factor num-shards)))
+                                                                           :DB_PORT (str mysql-port)})))
+                                   (lk/add-files-to-container :wait-for-dbs :scan-dbs "/etc/wait-for-db"
+                                                              {"scan-dbs.sh" "for i in $(seq 0 $NUM_NODES); do nc -z mdb-es-$i.mariadb-event-store $DB_PORT; done"})
+                                   (lk/add-container :respond "busybox"
+                                                     {:command ["sh" "-c" "while true; do nc -lp 8888; done"]})
+                                   (lk/deployment 1)
+                                   (lk/expose-cluster-ip :mdb-es-is-ready (lk/port :respond :db 8888 8888))))))))))
 
 
 '(-> (lk/injector)
